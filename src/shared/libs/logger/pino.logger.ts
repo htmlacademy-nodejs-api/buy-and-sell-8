258ecdaad8dbbed81e65d11f50220d9de33d7086
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-03 18:25:37 +0300

    3.25. Добавит вывод сообщения в `PinoLogger`.
    Зависимости внедряются и экземпляры нужных классов создаются автоматически. Это хорошо.
    
    Давайте попробуем подумать над вопросом: сколько экземпляров классов создаётся при внедрении зависимостей?
    
    Чтобы не гадать, откроем класс `PinoLogger` и в конструкторе добавим вывод сообщения: «Logger created…».
    
    Выходит, что при создании экземпляра класса в терминале мы должны увидеть эту строку. Попробуйте выполнить приложение и посчитать сколько
    раз она выводится.
    
    Как видите — дважды. При внедрении зависимости создаётся новый экземпляр логера. У нас он внедряется в два класса, поэтому создаётся два разных экземпляра.
    
    С одной стороны это не проблема. Ну создаётся и создаётся. С другой — лишние ресурсы. Далеко не во всех случаях нужно каждый раз создавать новый экземпляр класса.

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -30,6 +30,7 @@ export class PinoLogger implements Logger {
     });
 
     this.logger = pino({}, multiTransport);
+    this.logger.info('Logger created…');
   }
 
   public debug(message: string, ...args: unknown[]): void {
