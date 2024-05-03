55edc2711696192f8574d72611dd34018b8916d5
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-03 18:18:53 +0300

    3.20. Добавит перечисление `Component`.
    Подготовим новый модуль с перечислением компонентов нашего приложения, которые будут участвовать при внедрении зависимостей.
    
    Новый модуль создаём в директории `types`, хотя фактически это не является описание типа в контексте TypeScript. Мы описываем простой константный объект. Ключи объекта — название компонентов, а если точней
    интерфейсов. Значения ключей — символы. Можно использовать строки, но символы уникальны «из коробки».
    
    Объект нам потребуется при подготовке контейнера зависимостей.

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,3 +4,4 @@ export { User } from './user.type.js';
 export { Offer } from './offer.type.js';
 
 export { MockServerData } from './mock-server-data.type.js';
+export { Component } from './component.enum.js';
