d61921c1bb171e51796964ceb93d7804bd1dc1a7
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-08 18:46:18 +0300

    4.29. Добавит `DefaultCategoryService`.
    Следующим шагом реализуем сервис `DefaultCategoryService`. Для этого создадим
    отдельный класс, который имплементирует интерфейс `CategoryService`.
    
    Внутри этого класса нам потребуется доступ к логеру и модели `CategoryModel`. Внедрим их через зависимости. Для этого расширим объект
    `Component` и добавим новый контейнер `category.container.ts`. В этот контейнер добавим необходимые зависимости.
    
    Обратите внимание на реализацию метода `create`. В нём мы не создаём экземпляр `CategoryEntity`. В этом нет никакой необходимости, так как
    нам не требуется выполнять никакие методы у этой сущности. Создать
    новую категорию мы сразу можем на основании DTO.

--- src/shared/modules/category/index.ts
+++ src/shared/modules/category/index.ts
@@ -1,3 +1,5 @@
 export { CreateCategoryDto } from './dto/create-category.dto.js';
 export { CategoryService } from './category-service.interface.js';
 export { CategoryEntity, CategoryModel } from './category.entity.js';
+export { DefaultCategoryService } from './category.service.js';
+export { createCategoryContainer } from './category.container.js';
