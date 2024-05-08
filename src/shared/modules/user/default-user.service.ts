f94a4508394aaa6d38416eb6521b7afd3c688070
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-08 18:40:18 +0300

    4.26. Добавит внедрение модели в зависимости. WIP
    Класс `DefaultUserService` напрямую обращается к модели `UserModel`.
    Мы можем понизить эту связь и внедрять модель в качестве зависимости.
    Для этого нам потребуется проделать несколько шагов.
    
    Начнём с добавления нового компонента приложения в `Component`.
    Определим `UserModel`. Затем создадим новый контейнер `user.container.ts`
    и добавим его в общий контейнер. На этот раз мы воспользуемся методом
    `toConstantValue`. Дело в том, что нам не нужно создавать новый экземпляр
    модели. Мы напрямую сможем работать с `UserModel`.
    
    Затем вернёмся в `DefaultUserService` и внедрим модель в качестве зависимости.
    После этого сможем обращаться к модели через `this.userModel`.
    
    WIP: Имплементирован только один метод интерфейса `UserService`.
    Линтер ругается.

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -2,7 +2,7 @@ import { DocumentType } from '@typegoose/typegoose';
 import { inject, injectable } from 'inversify';
 
 import { UserService } from './user-service.interface.js';
-import { UserEntity, UserModel } from './user.entity.js';
+import { UserEntity } from './user.entity.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
@@ -11,13 +11,14 @@ import { Logger } from '../../libs/logger/index.js';
 export class DefaultUserService implements UserService {
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.UserModel) private readonly userModel: types.ModelType<UserEntity>
   ) {}
 
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
-    const result = await UserModel.create(user);
+    const result = await this.userModel.create(user);
     this.logger.info(`New user created: ${user.email}`);
 
     return result;
