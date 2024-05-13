33c278bbc69605bc1c891c9335d0c282160c6045
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-13 21:53:42 +0300

    5.13. Добавит `DefaultCommentService`.
    Следующим шагом подготовим всё необходимое для сохранения данных в
    базу. Начнём с интерфейса `CommentService`. Сервис должен уметь сохранять данные в базу (метод `create`) и извлекать комментарии для определённого объявления (метод `findByOfferId`).
    
    Сервиса будет внедряться в виде зависимости, поэтому сразу добавим
    информацию о нём и модели в контейнер зависимостей. Для этого добавим
    новый компонент (`Component`) и соответствующие привязки в `main.rest.ts`.

---
+++ src/shared/modules/comment/index.ts
@@ -0,0 +1,4 @@
+export { CommentService } from './comment-service.interface.js';
+export { createCommentContainer } from './comment.container.js';
+export { CommentEntity, CommentModel } from './comment.entity.js';
+export { DefaultCommentService } from './default-comment.service.js';
