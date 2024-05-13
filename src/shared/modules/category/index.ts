9fc0c86ac2fc935ebefd314c051327e4ebac90d8
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-13 21:55:59 +0300

    5.17. Добавит ограничение для вывода списка категорий и сортировку.
    Немного улучшим агрегацию. Во-первых, толку от поля `offers` никакого.
    Мы им воспользовались один раз, чтобы посчитать количество элементов.
    Больше в нём нет необходимости. Раз так, можно от него избавиться,
    чтобы не передавать лишние данные. Для этого воспользуемся оператором
    `$unset`. Значением передаём название поля, которое нужно удалить.
    
    Затем ограничим вывод списка категорий шестью. Для этого мы завели
    константу `MAX_CATEGORIES_COUNT`. Ограничение сделаем с помощью
    оператора `$limit`.
    
    Последним шагом выполним сортировку выборки. Отсортируем категории по
    убыванию. Полем для сортировки станет `offerCount`. Сначала будут
    категории с бОльшим количеством объявлений.
    
    Всё, на этом закончим с агрегацией.

--- src/shared/modules/category/index.ts
+++ src/shared/modules/category/index.ts
@@ -3,3 +3,4 @@ export { CategoryService } from './category-service.interface.js';
 export { CategoryEntity, CategoryModel } from './category.entity.js';
 export { DefaultCategoryService } from './category.service.js';
 export { createCategoryContainer } from './category.container.js';
+export { MAX_CATEGORIES_COUNT } from './category.constant.js';
