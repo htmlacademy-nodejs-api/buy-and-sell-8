8f72f74c5442554a16f878d957f90942f9c22f51
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-20 22:21:28 +0300

    6.19. Добавит контроллер `UserController`. WIP
    Протестируем `ExceptionFilter` в новом контроллере. Создадим контроллер `UserController` и реализуем в нём обработчики для ресурса `users`. Помните, каждый контроллер является потомком абстрактного класса `BaseController`.
    
    В `UserController` у нас будет несколько обработчиков: `/register` (для регистрации новых пользователей) и `/login` (для авторизации). Пока добавим один обработчик `/register` и создадим метод `create`. В нём бросим ошибку для проверки `AppExceptionFilter`.
    
    Добавим `UserController` в компоненты (`Components`) и создадим
    привязку в контейнере. Затем в `RestApplication` зарегистрируем новый контроллер.
    
    Затем добавим в `user.http` новый запрос для проверки обработчика регистрации. Попробуйте выполнить запрос к `/users/register`. Поскольку в обработчике мы бросили ошибку, её перехватит `AppExceptionFilter` и зафиксирует информацию об этом в логах, а также вернёт клиенту корректный код ответа.
    
    WIP: Добавит основу для контроллера `UserController`.

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -18,6 +18,7 @@ export class RestApplication {
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
     @inject(Component.CategoryController) private readonly categoryController: Controller,
     @inject(Component.ExceptionFilter) private readonly appExceptionFilter: ExceptionFilter,
+    @inject(Component.UserController) private readonly userController: Controller,
   ) {
     this.server = express();
   }
@@ -41,6 +42,7 @@ export class RestApplication {
 
   private async _initControllers() {
     this.server.use('/categories', this.categoryController.router);
+    this.server.use('/users', this.userController.router);
   }
 
   private async _initMiddleware() {
--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -4,3 +4,5 @@ export { Controller } from './controller/controller.interface.js';
 export { BaseController } from './controller/base-controller.abstract.js';
 export { ExceptionFilter } from './exception-filter/exception-filter.interface.js';
 export { AppExceptionFilter } from './exception-filter/app-exception-filter.js';
+export { RequestParams } from './types/request.params.type.js';
+export { RequestBody } from './types/request-body.type.js';
