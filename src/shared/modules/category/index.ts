4ae66ac701af462533d518bd8129f9aac3d38451
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-20 22:15:53 +0300

    6.13. Добавит объект ответа `category.rdo.ts`.
    Контроллер работает, но отдаёт данных больше, чем нужно. Клиенту не нужны `createdAt`, `updatedAt`, `_id` и `__v`. Для ресурса `category` достаточно `name` и `id`.
    
    Эту задачу можно решить несколькими способами. Первый: модифицировать обработчик `toJSON` для модели. В `mongoose` реализована эта возможность (смотри документацию). Второй: определить RDO с нужными полями. Этот способ более гибкий. Воспользуемся им.
    
    RDO объявим в виде класса, а создадим его с помощью `plainToInstance` из пакета `class-transformer`. Эту функцию придётся использовать часто, поэтому подготовим обёртку в виде `fillDTO`. Обратите внимание, в `CategoryRdo` мы применяем декоратор `@Expose`. Им мы отмечаем, что свойство должно быть включено для заполнения.

--- src/shared/modules/category/index.ts
+++ src/shared/modules/category/index.ts
@@ -5,3 +5,4 @@ export { DefaultCategoryService } from './category.service.js';
 export { createCategoryContainer } from './category.container.js';
 export { MAX_CATEGORIES_COUNT } from './category.constant.js';
 export { CategoryController } from './category.controller.js';
+export { CategoryRdo } from './rdo/category.rdo.js';
