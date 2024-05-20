c84163c0d28284dea180441735152fcdb3424dae
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-20 22:21:28 +0300

    6.19. Добавит контроллер `UserController`. WIP
    Протестируем `ExceptionFilter` в новом контроллере. Создадим контроллер `UserController` и реализуем в нём обработчики для ресурса `users`. Помните, каждый контроллер является потомком абстрактного класса `BaseController`.
    
    В `UserController` у нас будет несколько обработчиков: `/register` (для регистрации новых пользователей) и `/login` (для авторизации). Пока добавим один обработчик `/register` и создадим метод `create`. В нём бросим ошибку для проверки `AppExceptionFilter`.
    
    Добавим `UserController` в компоненты (`Components`) и создадим
    привязку в контейнере. Затем в `RestApplication` зарегистрируем новый контроллер.
    
    Затем добавим в `user.http` новый запрос для проверки обработчика регистрации. Попробуйте выполнить запрос к `/users/register`. Поскольку в обработчике мы бросили ошибку, её перехватит `AppExceptionFilter` и зафиксирует информацию об этом в логах, а также вернёт клиенту корректный код ответа.
    
    WIP: Добавит основу для контроллера `UserController`.

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -7,3 +7,4 @@ export { CreateUserDto } from './dto/create-user.dto.js';
 export { DefaultUserService } from './default-user.service.js';
 export { createUserContainer } from './user.container.js';
 export { UserService } from './user-service.interface.js';
+export { UserController } from './user.controller.js';
