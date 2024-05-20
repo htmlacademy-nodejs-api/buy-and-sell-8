ad094c8dfbffee4e61e6f5e3af93f0ca355fc131
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-20 22:16:18 +0300

    6.14. Добавит метод `_initMiddleware` в `RestApplication`.
    Обработчик создания новой категории реализуется похожим образом.
    Главное отличие: нужно извлечь данные из запроса клиента и вызвать соответствующие методы `CategoryService`.
    
    И вот здесь появляется первая сложность. Вы помните, что любой
    http-запрос — это обычный текст. Чтобы воспользоваться данными из запроса, необходимо разобрать тело запроса. В теле запроса могут находиться данные в разных форматах. Наш сервис ожидает данные в формате JSON, поэтому нужно научиться корректно извлекать JSON.
    
    Для решения этой задачи следует воспользоваться middleware. Это
    промежуточные обработчики, в которых может быть реализована
    дополнительная логика.
    
    Мы воспользуемся встроенным middleware — `express.json()`. Сделаем отдельный метод `_initMiddleware` в классе `RestApplication`. В  нём будем регистрировать все необходимые middleware.

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -42,12 +42,20 @@ export class RestApplication {
     this.server.use('/categories', this.categoryController.router);
   }
 
+  private async _initMiddleware() {
+    this.server.use(express.json());
+  }
+
   public async init() {
     this.logger.info('Application initialization');
 
     this.logger.info('Init database…');
     await this.initDb();
     this.logger.info('Init database completed');
+
+    this.logger.info('Init app-level middleware');
+    await this._initMiddleware();
+    this.logger.info('App-level middleware initialization completed');
 
     this.logger.info('Init controllers');
     await this._initControllers();
