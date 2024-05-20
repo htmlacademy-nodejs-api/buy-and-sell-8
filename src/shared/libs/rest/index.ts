e7786db4e8ba2517f2186bbcad52dec1d9751dcd
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-20 22:19:01 +0300

    6.17. Добавит класс `AppExceptionFilter`.
    Теперь добавим имплементацию `ExceptionFilter`. Для этого создадим класс `AppExceptionFilter`.
    
    В классе нам потребуется логер. Поэтому внедрим его сразу в виде
    зависимости через конструктор. В конструкторе добавим вывод информации о регистрации `AppExceptionFilter`.
    
    Затем имплементируем метод `catch`. Пока сделаем самый простой вариант. Сделаем запись в лог сообщения ошибки (`error.message`) и отправим клиенту корректный ответ. Помните, даже если в сервисе произошла ошибка, следует отправить клиенту корректный код. В случае неизвестной ошибки таким кодом станет `500`.
    
    Помимо кода с ошибкой отправим объект с ошибкой. Здесь можно пойти дальше и внедрить отправку дополнительных констант для автоматической обработки ошибок клиентом, но пока не будем переусложнять.

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -3,3 +3,4 @@ export { Route } from './types/route.interface.js';
 export { Controller } from './controller/controller.interface.js';
 export { BaseController } from './controller/base-controller.abstract.js';
 export { ExceptionFilter } from './exception-filter/exception-filter.interface.js';
+export { AppExceptionFilter } from './exception-filter/app-exception-filter.js';
