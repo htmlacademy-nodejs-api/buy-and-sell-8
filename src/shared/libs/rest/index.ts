cd326a6ecaab0e522a8c8d27c9fd1512f748d090
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-20 22:18:15 +0300

    6.16. Добавит интерфейс `ExceptionFilter`.
    `CategoryController` полностью готов, теперь пришло время подумать про обработку ошибок. От ошибок никуда не деться и они рано или поздно появится. Поэтому важно научиться их обрабатывать.
    
    Вы знаете, что для обработки ошибок достаточно научиться пользоваться исключениями. Но как быть с ошибками, которые могут возникнуть в обработчиках Express?
    
    Есть хорошая новость: Express из коробки умеет обрабатывать исключения, которые возникают в обработчиках. Если бросить ошибку в синхронном обработчике, то Express её автоматом поймает и отправит клиенту код `500` (Internal Server Error) и включит в ответ весь стек-трейс.
    
    Это неплохой вариант для начала, но клиенту обычно не нужен стек-трейс вызова функций, ему достаточно кода ошибки и понятного описания. Исправить стандартный обработчик ошибок легко, достаточно зарегистрировать собственный. В нём можно сделать более продвинутую обработку ошибок.
    
    Такие обработчики обычно называют `ExceptionFilter`. При переходе на другую технологию (например, ASP .NET Core) вы столкнётесь с аналогичным наименованием. Попробуем реализовать такой обработчик исключений в нашем проекте.
    
    Начнём с проектирования интерфейса `ExceptionFilter`. Предусмотрим в нём единственный метод `catch`. Сигнатура метода полностью соответствует сигнатуре обработчика ошибок в Express. Первым аргументом он принимает объект ошибки, а в остальных традиционный набор: объект запроса, объект ответа и функцию для передачи следующему обработчику.

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -2,3 +2,4 @@ export { HttpMethod } from './types/http-method.enum.js';
 export { Route } from './types/route.interface.js';
 export { Controller } from './controller/controller.interface.js';
 export { BaseController } from './controller/base-controller.abstract.js';
+export { ExceptionFilter } from './exception-filter/exception-filter.interface.js';
