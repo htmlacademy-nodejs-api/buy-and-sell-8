3870fd4dc91d460046ef171578005f4203f18814
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-28 18:02:05 +0300

    7.27. Добавит `middleware` для проверки наличия документа в коллекции.
    В обработчиках маршрутах, где в пути используется идентификатор документа в БД нам приходится делать одну и ту же проверку: проверить существование документа, а только потом выполнять основную операцию. В этом нет ничего страшного, но код контроллеров немного раздувается и появляется дополнительный шум.
    
    Можно немного сократить объём кода, если перенести задачу проверки сущности в `middleware`. Проверим на практике. Создадим новую middleware `DocumentExistsMiddleware`.
    
    Чтобы из middleware проверить наличие документа в базе нам потребуется
    определённый сервис. Общего интерфейса для всех сервисов у нас нет. Можно перечислить все созданные сервисы, но этот вариант не сильно гибкий. Придётся поддерживать список сервисов в актуальном виде, да ещё проследить, чтобы у всех сервисов был метод `exists`.
    
    Оптимальный создать отдельный интерфейс `DocumentExistsInterface` и в нём описать метод `exists`. Поскольку метод `exists` не привязан к определённой сущности, мы сможем все интерфейсы сервисов, где требуется реализация метода — унаследовать от него.

---
+++ src/shared/libs/rest/middleware/document-exists.middleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { Middleware } from './middleware.interface.js';
+import { DocumentExists } from '../../../types/index.js';
+import { HttpError } from '../errors/index.js';
+
+export class DocumentExistsMiddleware implements Middleware {
+  constructor(
+    private readonly service: DocumentExists,
+    private readonly entityName: string,
+    private readonly paramName: string,
+  ) {}
+
+  public async execute({ params }: Request, _res: Response, next: NextFunction): Promise<void> {
+    const documentId = params[this.paramName];
+    if (! await this.service.exists(documentId)) {
+      throw new HttpError(
+        StatusCodes.NOT_FOUND,
+        `${this.entityName} with ${documentId} not found.`,
+        'DocumentExistsMiddleware'
+      );
+    }
+
+    next();
+  }
+}
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -6,3 +6,4 @@ export { Offer } from './offer.type.js';
 export { MockServerData } from './mock-server-data.type.js';
 export { Component } from './component.enum.js';
 export { SortType } from './sort-type.enum.js';
+export { DocumentExists } from './document-exists.interface.js';
