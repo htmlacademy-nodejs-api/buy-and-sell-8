d464e0bf7df7ac26ebd04f3f0c1d8e4878083062
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-28 13:43:10 +0300

    7.7. Добавит метод `update` в `OfferController`.
    Разберём кейс с редактированием объявлений. Реализовать редактирование
    с точки зрения API можно несколькими способами. Один из очевидных: реализовать обработку PUT-запросов. Метод `PUT` подразумевает полное
    обновление.
    
    Есть другой вариант, более гибкий — `PATCH`. Этот метод подразумевает
    частичное обновление. Реализуем его в `OfferController`.
    
    Для обновления документа в коллекции расширим интерфейс `OfferService`. Добавим метод `updateById`. Для обновления воспользуемся методом `findByIdAndUpdate`.
    
    По традиции добавим запрос для проверки в `offer.http`.

--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -10,6 +10,7 @@ import { Logger } from '../../libs/logger/index.js';
 import { fillDTO } from '../../helpers/index.js';
 import { OfferRdo } from './rdo/offer.rdo.js';
 import { CreateOfferRequest } from './type/create-offer-request.type.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
 
 @injectable()
 export default class OfferController extends BaseController {
@@ -24,6 +25,7 @@ export default class OfferController extends BaseController {
     this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
     this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
     this.addRoute({ path: '/:offerId', method: HttpMethod.Delete, handler: this.delete });
+    this.addRoute({ path: '/:offerId', method: HttpMethod.Patch, handler: this.update });
   }
 
   public async show({ params }: Request<ParamOfferId>, res: Response): Promise<void> {
@@ -66,4 +68,19 @@ export default class OfferController extends BaseController {
 
     this.noContent(res, offer);
   }
+
+  public async update({ body, params }: Request<ParamOfferId, unknown, UpdateOfferDto>, res: Response): Promise<void> {
+    const updatedOffer = await this.offerService.updateById(params.offerId, body);
+
+    if (!updatedOffer) {
+      throw new HttpError(
+        StatusCodes.NOT_FOUND,
+        `Offer with id ${params.offerId} not found.`,
+        'OfferController'
+      );
+    }
+
+    this.ok(res, fillDTO(OfferRdo, updatedOffer));
+  }
 }
+
