8addf5e971675d3f87e9ea08b4fbb5ea2c88fb98
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-28 13:46:34 +0300

    7.9. Добавит контроллер `CommentController`. WIP
    Теперь реализуем `CommentController`. В нём потребуется один обработчик
    маршрута — `create`. Он отвечает за добавление нового комментария. При
    реализации повторяем уже знакомую технику: проверяем наличие документа
    с объявлением и если документ существует, создаём комментарий.
    
    Обратите внимание, здесь мы используем метод `exists` у `OfferService`.
    Этот метод мы ещё не разбирали и реализуем его в следующем шаге.
    
    Создание комментария ничем не отличается от создания объявления —
    вызываем метод `create` и передаём данные из тела запроса. Затем отдаём
    пользователю результат. По традиции готовим отдельную DTO — `CommentDto`.
    
    Чтобы воспользоваться `CommentController`, его необходимо добавить в
    компоненты приложения (`AppComponent`) и в контейнер с зависимостями
    (`main.ts`). После этого можно внедрять в качестве зависимости в
    `application` и регистрировать в методе `registerRoutes`.
    
    WIP. Линтер ругается на отсутствующий метод `exists` у `OfferService`.

--- src/shared/modules/comment/index.ts
+++ src/shared/modules/comment/index.ts
@@ -2,3 +2,5 @@ export { CommentService } from './comment-service.interface.js';
 export { createCommentContainer } from './comment.container.js';
 export { CommentEntity, CommentModel } from './comment.entity.js';
 export { DefaultCommentService } from './default-comment.service.js';
+export { CreateCommentDto } from './dto/create-comment.dto.js';
+export { CommentRdo } from './rdo/comment.rdo.js';
