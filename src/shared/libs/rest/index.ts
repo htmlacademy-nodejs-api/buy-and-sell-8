85abdd3d935f843ffb7f177afb392ce1a61fa6e3
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-28 13:52:42 +0300

    7.20. Добавит `ValidateDtoMiddleware`.
    Следующим шагом следует решить, где выполнять проверку валидации
    данных. Удобней всего это делать в middleware. Если DTO не получилось
    провалидировать, можно сразу вернуть ошибку и обработка запроса на
    этом прервётся. Клиент получит всю необходимую информацию об ошибке.
    
    Создадим новую middleware `ValidateDtoMiddleware`. Класс должен
    реализовать интерфейс `MiddlewareInterface`. Для выполнения проверки необходимо вызвать функцию `validate` из пакета `class-validator`.
    Аргументом необходимо передать экземпляр класса, который необходимо провалидировать.
    
    Здесь у нас появляется первая сложность. Чтобы создать экземпляр класса, необходимо знать, какой именно. Делать отдельную middleware для каждой DTO неразумно. Поэтому нужную DTO будем передавать через конструктор. Предусмотрим для этого соответствующее приватное поле `dto`. С созданием экземпляра класса сложностей нет. Для этого мы можем воспользоваться функцией `plainToInsurance` из пакета `class-transformer`. Мы уже использовали её в проекте.
    
    После этого можно передать экземпляр созданного класса в функцию
    `validate`. Результатом выполнения станет массив ошибок или пустой массив. Мы делаем проверку, если в массиве что-то есть, значит есть ошибки. Поэтому нужно отправить их клиенту. Пока отправляем массив целиком, чуть позже мы сократим объём передаваемой информации.
    
    Если ошибок нет, то нужно передать работу следующему обработчику или middleware. Вызываем функцию `next`.

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -10,3 +10,4 @@ export { HttpError } from './errors/index.js';
 export { RequestQuery } from './types/request-query.type.js';
 export { Middleware } from './middleware/middleware.interface.js';
 export { ValidateObjectIdMiddleware } from './middleware/validate-objectid.middleware.js';
+export { ValidateDtoMiddleware } from './middleware/validate-dto.middleware.js';
