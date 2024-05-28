31b4ac584671b8de7ebe8524b7c1ff68ea55b38f
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-28 13:38:50 +0300

    7.1. Добавит контроллер `OfferController`. WIP
    Добавим новый контроллер `OfferController`. Он будет отвечать за
    обработку запросов создания, чтения и редактирования объявлений. Начнём с подготовки основы. Отнаследуемся от `Controller`. Затем определим первый маршрут — `/:offerId`. Чуть позже реализуем в нём получение объявления из базы данных, а пока сделаем заглушку.
    
    Следующим шагом добавим новый элемент в `Component` и в контейнер.
    После этого внедрим контроллер `OfferController` в `RestApplication` и
    зарегистрируем в методе `_initControllers`.
    
    WIP: Контроллер в разработке.

--- src/shared/modules/offer/offer.container.ts
+++ src/shared/modules/offer/offer.container.ts
@@ -6,11 +6,15 @@ import { Component } from '../../types/index.js';
 import { DefaultOfferService } from './default-offer.service.js';
 import { OfferEntity, OfferModel } from './offer.entity.js';
 
+import OfferController from './offer.controller.js';
+import { Controller } from '../../libs/rest/index.js';
+
 export function createOfferContainer() {
   const offerContainer = new Container();
 
   offerContainer.bind<OfferService>(Component.OfferService).to(DefaultOfferService);
   offerContainer.bind<types.ModelType<OfferEntity>>(Component.OfferModel).toConstantValue(OfferModel);
+  offerContainer.bind<Controller>(Component.OfferController).to(OfferController).inSingletonScope();
 
   return offerContainer;
 }
---
+++ src/shared/modules/offer/offer.controller.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from 'inversify';
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { BaseController, HttpError, HttpMethod } from '../../libs/rest/index.js';
+import { Component } from '../../types/index.js';
+import { Logger } from '../../libs/logger/index.js';
+
+@injectable()
+export default class OfferController extends BaseController {
+  constructor(
+    @inject(Component.Logger) logger: Logger,
+  ) {
+    super(logger);
+
+    this.logger.info('Register routes for OfferController');
+    this.addRoute({ path: '/:offerId', method: HttpMethod.Get, handler: this.show });
+  }
+
+  public async show(_req: Request, _res: Response): Promise<void> {
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented',
+      'OfferController'
+    );
+  }
+}
