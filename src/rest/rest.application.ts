ebffc846cbf9968f4af42a165a0494f55a989d76
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-28 13:38:50 +0300

    7.1. Добавит контроллер `OfferController`. WIP
    Добавим новый контроллер `OfferController`. Он будет отвечать за
    обработку запросов создания, чтения и редактирования объявлений. Начнём с подготовки основы. Отнаследуемся от `Controller`. Затем определим первый маршрут — `/:offerId`. Чуть позже реализуем в нём получение объявления из базы данных, а пока сделаем заглушку.
    
    Следующим шагом добавим новый элемент в `Component` и в контейнер.
    После этого внедрим контроллер `OfferController` в `RestApplication` и
    зарегистрируем в методе `_initControllers`.
    
    WIP: Контроллер в разработке.

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -19,6 +19,7 @@ export class RestApplication {
     @inject(Component.CategoryController) private readonly categoryController: Controller,
     @inject(Component.ExceptionFilter) private readonly appExceptionFilter: ExceptionFilter,
     @inject(Component.UserController) private readonly userController: Controller,
+    @inject(Component.OfferController) private readonly offerController: Controller,
   ) {
     this.server = express();
   }
@@ -43,6 +44,7 @@ export class RestApplication {
   private async _initControllers() {
     this.server.use('/categories', this.categoryController.router);
     this.server.use('/users', this.userController.router);
+    this.server.use('/offers', this.offerController.router);
   }
 
   private async _initMiddleware() {
