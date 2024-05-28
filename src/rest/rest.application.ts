d22f8f214b787f0d80978c06f39161ceef6bb3e8
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-28 13:46:34 +0300

    7.9. Добавит контроллер `CommentController`. WIP
    Теперь реализуем `CommentController`. В нём потребуется один обработчик
    маршрута — `create`. Он отвечает за добавление нового комментария. При
    реализации повторяем уже знакомую технику: проверяем наличие документа
    с объявлением и если документ существует, создаём комментарий.
    
    Обратите внимание, здесь мы используем метод `exists` у `OfferService`.
    Этот метод мы ещё не разбирали и реализуем его в следующем шаге.
    
    Создание комментария ничем не отличается от создания объявления —
    вызываем метод `create` и передаём данные из тела запроса. Затем отдаём
    пользователю результат. По традиции готовим отдельную DTO — `CommentDto`.
    
    Чтобы воспользоваться `CommentController`, его необходимо добавить в
    компоненты приложения (`AppComponent`) и в контейнер с зависимостями
    (`main.ts`). После этого можно внедрять в качестве зависимости в
    `application` и регистрировать в методе `registerRoutes`.
    
    WIP. Линтер ругается на отсутствующий метод `exists` у `OfferService`.

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -20,6 +20,7 @@ export class RestApplication {
     @inject(Component.ExceptionFilter) private readonly appExceptionFilter: ExceptionFilter,
     @inject(Component.UserController) private readonly userController: Controller,
     @inject(Component.OfferController) private readonly offerController: Controller,
+    @inject(Component.CommentController) private readonly commentController: Controller,
   ) {
     this.server = express();
   }
@@ -45,6 +46,7 @@ export class RestApplication {
     this.server.use('/categories', this.categoryController.router);
     this.server.use('/users', this.userController.router);
     this.server.use('/offers', this.offerController.router);
+    this.server.use('/comments', this.commentController.router);
   }
 
   private async _initMiddleware() {
