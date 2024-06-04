fd83143f71296963f028130f9dc538da98fe3328
Igor Antonov <antonov.igor.khv@gmail.com>
2024-06-04 13:18:43 +0300

    8.7. Подготовим контейнер зависимостей для модуля `Auth`. WIP
    Теперь подготовим инфраструктуру для нового модуля `Auth`. Соберём все необходимые зависимости в контейнере, внедрим нужные компоненты в `RestApplication`, зарегистрируем новый фильтр исключений, добавим компоненты в `Component` и так далее.

---
+++ src/shared/modules/auth/index.ts
@@ -0,0 +1,4 @@
+export { AuthService } from './auth-service.interface.js';
+export { TokenPayload } from './types/TokenPayload.js';
+export { createAuthContainer } from './auth.container.js';
+export { DefaultAuthService } from './default-auth.service.js';
