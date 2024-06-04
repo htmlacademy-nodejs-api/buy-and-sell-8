2ddac4c3dfb910ddaa7fbab0abe94117efc51982
Igor Antonov <antonov.igor.khv@gmail.com>
2024-06-04 13:16:09 +0300

    8.4. Добавит `AuthService` и `DefaultAuthService`. WIP
    Для реализации проверки логина и пароля, а также формирования токена подготовим отдельный модуль `Auth`. В модуле предусмотрим методы
    
    * `authenticate`. Аргументом принимает `UserEntity`. Результатом становится токен.
    * `verify`.  Выполняет аутентификацию. На вход принимает `LoginUserDto`. Результатом станет `UserEntity` или ошибка.
    
    В этом же коммите добавим константы `JWT_ALGORITHM` и `JWT_EXPIRED`. В первой зафиксируем алгоритм для формирования токена, а во второй время, в течение которого токен считается валидным.
    
    WIP: Задача в процесс решения.

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -8,3 +8,4 @@ export { DefaultUserService } from './default-user.service.js';
 export { createUserContainer } from './user.container.js';
 export { UserService } from './user-service.interface.js';
 export { UserController } from './user.controller.js';
+export { LoginUserDto } from './dto/login-user.dto.js';
