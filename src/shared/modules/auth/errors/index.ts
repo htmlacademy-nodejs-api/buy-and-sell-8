050e806f9b05780bc17ad1d38f63fc149d4e2e96
Igor Antonov <antonov.igor.khv@gmail.com>
2024-06-04 13:17:16 +0300

    8.5. Новые ошибки `UserNotFoundException` и `UserPasswordIncorrectException`. WIP
    При выполнении процедуры аутентификации могут возникнуть различные ошибки. Например, пользователь может быть не обнаружен в базе данных. Или его пароль неправильный. Это только два частых кейса, но их быть может быть много.
    
    Есть разные способы обработки подобных ситуаций. Методы сервиса `Auth` могут возвращать, например, значение `null`, если пользователь не найден. Можно обработать это значение где-то в месте обращения к серверу и подготовить соответствующий ответ клиенту.
    
    Можно пойти другим путём и описать отдельные виды ошибок. Определяем для каждой ситуации ошибки, бросаем их при наступлении определённого события и дальше где-нибудь их обрабатываем.

---
+++ src/shared/modules/auth/errors/index.ts
@@ -0,0 +1,3 @@
+export { BaseUserException } from './base-user.exception.js';
+export { UserNotFoundException } from './user-not-found.exception.js';
+export { UserPasswordIncorrectException } from './user-password-incorrect.exception.js';
