9c0b87f8ac16d09491beefe57f5875bf0fbd90ce
Igor Antonov <antonov.igor.khv@gmail.com>
2024-06-07 12:57:21 +0300

    9.6. Добавит в конфигурацию переменную `HOST`.
    Следующим шагом добавим в конфигурацию новую переменную `HOST`. Пока имя хоста, на котором запущен сервис, статично. Добавим отдельную переменную `HOST` в конфигурацию. Несколькими шагами позже мы добавим возможность отдавать полный путь к статическим ресурсам. Поэтому возможность переопределять и где-то задавать хост нам пригодится. Предусмотрим в конфигурации значение по умолчанию — `localhost`.
    
    Чтобы упростить получение полного имени хоста, добавим отдельную функцию `getFullServerPath` и воспользуемся ей в `application`.

--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -36,3 +36,7 @@ export function reduceValidationErrors(errors: ValidationError[]): ValidationErr
     messages: constraints ? Object.values(constraints) : []
   }));
 }
+
+export function getFullServerPath(host: string, port: number) {
+  return `http://${host}:${port}`;
+}
