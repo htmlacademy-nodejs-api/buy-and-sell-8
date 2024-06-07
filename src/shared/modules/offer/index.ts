e8052542a61248ec0403eb0bdd12e90dfd93ca7e
Igor Antonov <antonov.igor.khv@gmail.com>
2024-06-07 13:02:20 +0300

    9.14. Добавит поддержку загрузки изображений для объявлений.
    К каждому объявлению клиент может загрузить изображение. Добавим поддержку загрузки изображений по аналогии с загрузкой аватарки пользователя. В контроллере `OfferController` добавим обработчик
    `uploadImage`.
    
    Ещё нам потребуется DTO, чтобы отдавать клиенту информацию о загруженном изображении (`upload-image.rdo.ts`).
    
    Последним шагом внесём изменения в `CreateOfferDto`. В поле `image` больше нет необходимости. В `offer.http` добавим новый запрос для проверки загрузки изображений.

--- src/shared/modules/offer/index.ts
+++ src/shared/modules/offer/index.ts
@@ -5,3 +5,4 @@ export { DefaultOfferService } from './default-offer.service.js';
 export { createOfferContainer } from './offer.container.js';
 export { OfferRdo } from './rdo/offer.rdo.js';
 export { CreateOfferValidationMessage } from './dto/create-offer.messages.js';
+export { UploadImageRdo } from './rdo/upload-image.rdo.js';
