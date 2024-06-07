fb47346a5bd9367f3314b7e29bab3e3b0eb93d42
Igor Antonov <antonov.igor.khv@gmail.com>
2024-06-07 12:31:57 +0300

    9.1. Добавит функцию для трансформации списка ошибок.
    При ошибках валидации `class-validator` возвращает большой объём информации. Он полезен на этапе отладки, но отправлять его клиенту избыточно. Попробуем немного урезать. Создадим функцию `reduceValidationErrors`. В ней сформируем объект с ошибкой валидации. Включим в него имя поля с ошибкой, его значение и список сообщений об ошибках.

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -15,3 +15,4 @@ export { DocumentExistsMiddleware } from './middleware/document-exists.middlewar
 export { UploadFileMiddleware } from './middleware/upload-file.middleware.js';
 export { ParseTokenMiddleware } from './middleware/parse-token.middleware.js';
 export { PrivateRouteMiddleware } from './middleware/private-route.middleware.js';
+export { ValidationErrorField } from './types/validation-error-field.type.js';
