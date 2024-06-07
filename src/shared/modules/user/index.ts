6ed0158e0912e582c8de351678f420a0d4b0c9e6
Igor Antonov <antonov.igor.khv@gmail.com>
2024-06-07 12:55:40 +0300

    9.5. Установит значение по умолчанию для поля `avatarPath`.
    Для загрузки изображения предусмотрен отдельный маршрут. Изображение аватара добавляется после создание пользователя. Поэтому удалим из
    `CreateUserDto` поле `avatarPath`.
    
    Затем немного модифицируем сервис `DefaultUserService`. Добавим в качестве аватара, файл по умолчанию. Имя файла с аватаром по умолчанию сохраним в константе `DEFAULT_AVATAR_FILE_NAME`.

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -9,3 +9,4 @@ export { createUserContainer } from './user.container.js';
 export { UserService } from './user-service.interface.js';
 export { UserController } from './user.controller.js';
 export { LoginUserDto } from './dto/login-user.dto.js';
+export { DEFAULT_AVATAR_FILE_NAME } from './user.constant.js';
