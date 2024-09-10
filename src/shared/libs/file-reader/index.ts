27b3160d728e4333edec4ea024bf409bd9571aa8
Igor Antonov <antonov.igor.khv@gmail.com>
2024-09-10 07:16:07 +0300

    1.12. Добавит класс `TSVFileReader`.
    Создадим класс `TSVFileReader`. В нём реализуем чтение и разбор tsv-файла.
    
    Воспользуемся той же практикой — синхронной функцией `readFileSync`. Она прочитает файл целиком.
    
    Такой подход хорош, когда файлы небольшие, но с большими файлами мы разберёмся позже.
    
    Мы считываем файл целиком и разбиваем его на строки. В этом нам поможет управляющий символ `\n`.
    
    Затем каждую строку разбиваем на отдельные части.

---
+++ src/shared/libs/file-reader/index.ts
@@ -0,0 +1,2 @@
+export { FileReader } from './file-reader.interface.js';
+export { TSVFileReader } from './tsv-file-reader.js';
---
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -0,0 +1,73 @@
+import { readFileSync } from 'node:fs';
+
+import { FileReader } from './file-reader.interface.js';
+import { Offer, OfferType, User } from '../../types/index.js';
+
+export class TSVFileReader implements FileReader {
+  private rawData = '';
+
+  constructor(
+    private readonly filename: string
+  ) {}
+
+  private validateRawData(): void {
+    if (! this.rawData) {
+      throw new Error('File was not read');
+    }
+  }
+
+  private parseRawDataToOffers(): Offer[] {
+    return this.rawData
+      .split('\n')
+      .filter((row) => row.trim().length > 0)
+      .map((line) => this.parseLineToOffer(line));
+  }
+
+  private parseLineToOffer(line: string): Offer {
+    const [
+      title,
+      description,
+      createdDate,
+      image,
+      type,
+      price,
+      categories,
+      firstname,
+      lastname,
+      email,
+      avatarPath
+    ] = line.split('\t');
+
+    return {
+      title,
+      description,
+      postDate: new Date(createdDate),
+      image,
+      type: OfferType[type as 'Buy' | 'Sell'],
+      categories: this.parseCategories(categories),
+      price: this.parsePrice(price),
+      user: this.parseUser(firstname, lastname, email, avatarPath),
+    };
+  }
+
+  private parseCategories(categoriesString: string): { name: string }[] {
+    return categoriesString.split(';').map((name) => ({ name }));
+  }
+
+  private parsePrice(priceString: string): number {
+    return Number.parseInt(priceString, 10);
+  }
+
+  private parseUser(firstname: string, lastname: string, email: string, avatarPath: string): User {
+    return { email, firstname, lastname, avatarPath };
+  }
+
+  public read(): void {
+    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+  }
+
+  public toArray(): Offer[] {
+    this.validateRawData();
+    return this.parseRawDataToOffers();
+  }
+}
