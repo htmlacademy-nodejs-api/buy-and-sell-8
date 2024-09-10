b8941b1ece2c102ea48cc629a557594dd2520aab
Igor Antonov <antonov.igor.khv@gmail.com>
2024-09-10 07:22:10 +0300

    2.5. Добавит тип `MockServerData`.
    В прошлом разделе мы договорились: получать исходный набор данных для формирования моков с отдельного сервиса. В роли такого сервиса станет JSON-server. Для него уже подготовлен файл с основой.
    
    Чтобы взаимодействовать с результатом, подготовим описание типа. Для этого создадим отдельный модуль `mock-server-data.type.ts` в директории `types`. В типе определим форму объекта, который возвращает сервер.

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,3 +2,5 @@ export { Category } from './category.type.js';
 export { OfferType } from './offer-type.enum.js';
 export { User } from './user.type.js';
 export { Offer } from './offer.type.js';
+
+export { MockServerData } from './mock-server-data.type.js';
