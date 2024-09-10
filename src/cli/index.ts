262d8e7f5a0e350a0dad0f083e31261f9089380e
Igor Antonov <antonov.igor.khv@gmail.com>
2024-09-10 07:16:27 +0300

    1.14. Зарегистрируем команду `ImportCommand`.
    Чтобы команда `--import` заработала, зарегистрируем её в `CLIApplication`, а затем попробуем протестировать:
    
    ```
    npm run ts ./src/main.cli.ts -- --import <path-to-mock>
    ```

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,3 +3,4 @@ export { CommandParser } from './command-parser.js';
 
 export { HelpCommand } from './commands/help.command.js';
 export { VersionCommand } from './commands/version.command.js';
+export { ImportCommand } from './commands/import.command.js';
