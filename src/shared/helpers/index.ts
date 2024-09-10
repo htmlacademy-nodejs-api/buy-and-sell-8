886259a604269229858984a53d617cb01812767c
Igor Antonov <antonov.igor.khv@gmail.com>
2024-09-10 07:27:58 +0300

    3.7. Добавит поддержку логирования в файлы.
    Pino может логировать не только в стандартный поток вывода. Пунктом назначения может выступать файл, база данных, онлайн-сервис и так далее. Для Pino существует множество готовых транспортов — плагинов, которые определяют как будут хранится логи.
    
    Попробуем вывести логи в файл. На текущем этапе путь к файлу с логами укажем прямо в коде.

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -4,3 +4,5 @@ export {
   getRandomItem,
   getErrorMessage,
 } from './common.js';
+
+export { getCurrentModuleDirectoryPath } from './file-system.js';
--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,12 +1,23 @@
-import { Logger as PinoInstance, pino } from 'pino';
+import { Logger as PinoInstance, pino, transport } from 'pino';
+import { resolve } from 'node:path';
 
 import { Logger } from './logger.interface.js';
+import { getCurrentModuleDirectoryPath } from '../../helpers/index.js';
 
 export class PinoLogger implements Logger {
   private readonly logger: PinoInstance;
 
   constructor() {
-    this.logger = pino();
+    const modulePath = getCurrentModuleDirectoryPath();
+    const logFilePath = 'logs/rest.log';
+    const destination = resolve(modulePath, '../../../', logFilePath);
+
+    const fileTransport = transport({
+      target: 'pino/file',
+      options: { destination }
+    });
+
+    this.logger = pino({}, fileTransport);
   }
 
   public debug(message: string, ...args: unknown[]): void {
