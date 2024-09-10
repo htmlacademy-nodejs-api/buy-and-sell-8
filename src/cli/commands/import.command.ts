490ac8bfa2e2b62eb760adae65a0f9d74bb60f3c
Igor Antonov <antonov.igor.khv@gmail.com>
2024-09-10 07:25:11 +0300

    2.16. Рефакторит команду `ImportCommand`.
    Последним шагом обновим класс с командой `Import`. В методе `execute` создадим экземпляр класса `TSVFileReader` и подпишемся на события `line` и `end`. Обработчиками событий станут приватные методы `onLine` и `onComplete`.
    
    Пока нет хранилища для долгосрочного хранения данных, отобразим импортированные объявления в консоль.
    
    Обрабатывать и выводить информацию об ошибках приходится часто. Чтобы не дублировать код для проверки экземпляра `error` на принадлежность
    к `Error`, создадим небольшую функцию-хелпер — `getErrorMessage`.

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,26 +1,33 @@
 import { Command } from './command.interface.js';
 import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
+import { Offer } from '../../shared/types/offer.type.js';
+import { getErrorMessage } from '../../shared/helpers/index.js';
 
 export class ImportCommand implements Command {
+  private onImportedOffer(offer: Offer): void {
+    console.info(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    console.info(`${count} rows imported.`);
+  }
+
   public getName(): string {
     return '--import';
   }
 
-  public execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [filename] = parameters;
     const fileReader = new TSVFileReader(filename.trim());
 
+    fileReader.on('line', this.onImportedOffer);
+    fileReader.on('end', this.onCompleteImport);
+
     try {
       fileReader.read();
-      console.log(fileReader.toArray());
-    } catch (err) {
-
-      if (!(err instanceof Error)) {
-        throw err;
-      }
-
+    } catch (error) {
       console.error(`Can't import data from file: ${filename}`);
-      console.error(`Details: ${err.message}`);
+      console.error(getErrorMessage(error));
     }
   }
 }
