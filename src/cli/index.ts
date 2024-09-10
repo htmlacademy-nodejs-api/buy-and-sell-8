9c7362d7bfdb47c0879e13529398a7671737de70
Igor Antonov <antonov.igor.khv@gmail.com>
2024-09-10 07:23:08 +0300

    2.8. Добавит команду формирования объявлений. WIP
    Для запуска генератора объявлений подготовим новую команду — `GenerateCommand`. Как и предыдущие аналогичные команды, класс команды должен реализовать интерфейс `Command`.
    
    Команда `GenerateCommand` ожидает несколько аргументов:
    
    * `<count>` — обязательный. Количество объявлений для генерации.
    * `<filepath>` — обязательный. Путь к файлу для записи результата.
    * `<url>` — обязательный. URL сервиса (JSON-server).
    
    Вызов команды должен выглядеть так:
    
    ```
    $: ./cli.js --generate 10 ./mocks/mock-offers.tsv http://localhost:3123/api
    ```
    
    На первом шаге создадим заготовку для команды `GenerateCommand`. В методе `execute` подготовим значения, полученные от пользователя.
    
    Обратите внимание, все данные от пользователя приходят в виде строк. Важно не забыть привести данные к нужному типу (при необходимости).
    
    WIP: Линтер ругается на неиспользуемые переменные.

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,3 +4,4 @@ export { CommandParser } from './command-parser.js';
 export { HelpCommand } from './commands/help.command.js';
 export { VersionCommand } from './commands/version.command.js';
 export { ImportCommand } from './commands/import.command.js';
+export { GenerateCommand } from './commands/generate.command.js';
